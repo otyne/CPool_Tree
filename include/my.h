9de1105b8f8448a975645cf8d51e9681d8560975
Otyne <43828915+otyne@users.noreply.github.com>
2018-10-04 22:02:42 +0200

    Add files

---
+++ include/my.h
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2018
+** my.h
+** File description:
+** my H
+*/
+
+void my_putchar(char);
+void tree(int);
---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** main.c
+** File description:
+** main.c
+*/
+
+#include "my.h"
+#include <stdlib.h>
+
+int main (int ac, char **av)
+{
+    tree(atoi(av[1]));
+}
